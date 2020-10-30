d10f4614e31701a216cd894f34b0d12508525f9b
Irina Yakubovskaya <juanacardo@gmail.com>
2020-10-30 17:49:28 +0500

    Оживляем интерактивные элементы
    Дополнения

--- js/script.js
+++ js/script.js
@@ -5,22 +5,49 @@ const children = modal.querySelector('.input-children');
 const modalForm = modal.querySelector('form');
 const dateIn = modal.querySelector('.input-in');
 
+let isStorageSupport = true;
+let adultsStorage = '';
+let childrenStorage = '';
+
+try {
+  adultsStorage = localStorage.getItem("adults");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+try {
+  childrenStorage = localStorage.getItem("children");
+} catch (err) {
+  isStorageSupport = false;
+}
+
 hotelSearchButton.addEventListener('click', function () {
-  modal.classList.toggle('modal-close');
+  modal.classList.toggle('modal-show');
+  modal.classList.remove('modal-error');
+  if (adultsStorage) {
+  adults.value = adultsStorage;
+  }
+  if (childrenStorage) {
+  children.value = childrenStorage;
+  }
   dateIn.focus();
-  });
+});
 
 modalForm.addEventListener('submit', function (evt) {
   if (!adults.value) {
-   evt.preventDefault();
+    evt.preventDefault();
+    modal.classList.add('modal-error');
   } else {
-      localStorage.setItem('adults', adults.value);
-      localStorage.setItem('children', children.value);
+    if (isStorageSupport) {
+    localStorage.setItem('adults', adults.value);
+    localStorage.setItem('children', children.value);
+    }
   }
 });
 
 modal.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
-    modal.classList.add('modal-close');
+  modal.classList.remove('modal-show');
+  modal.classList.remove('modal-error');
   }
 });
