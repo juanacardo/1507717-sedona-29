7ee11161e493caf4645961f1c32e2f39e402902e
Irina Yakubovskaya <juanacardo@gmail.com>
2020-10-31 22:02:19 +0300

    Оживляем интерактивные элементы
    Доработки

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 const hotelSearchButton = document.querySelector('.hotel-search-button');
 const modal = document.querySelector('.modal');
+const hotelSubmitButton = modal.querySelector('.modal-button');
 const adults = modal.querySelector('.input-adults');
 const children = modal.querySelector('.input-children');
 const modalForm = modal.querySelector('form');
 const dateIn = modal.querySelector('.input-in');
+const dateOut = modal.querySelector('.input-out');
 
 let isStorageSupport = true;
 let adultsStorage = '';
 let childrenStorage = '';
+
+modal.classList.remove('modal-show');
 
 try {
   adultsStorage = localStorage.getItem("adults");
@@ -22,32 +26,35 @@ try {
 }
 
 hotelSearchButton.addEventListener('click', function () {
-  modal.classList.toggle('modal-show');
+  modal.classList.toggle('modal-down');
   modal.classList.remove('modal-error');
   if (adultsStorage) {
-  adults.value = adultsStorage;
+    adults.value = adultsStorage;
   }
   if (childrenStorage) {
-  children.value = childrenStorage;
+    children.value = childrenStorage;
   }
-  dateIn.focus();
 });
 
 modalForm.addEventListener('submit', function (evt) {
-  if (!adults.value) {
-    evt.preventDefault();
-    modal.classList.add('modal-error');
-  } else {
-    if (isStorageSupport) {
+  if (isStorageSupport) {
     localStorage.setItem('adults', adults.value);
     localStorage.setItem('children', children.value);
-    }
+  }
+});
+
+hotelSubmitButton.addEventListener('click', function (evt) {
+if (!adults.value || !dateIn.value || !dateOut.value) {
+    evt.preventDefault();
+    modal.classList.remove('modal-error');
+    modal.offsetWidth = modal.offsetWidth;
+    modal.classList.add("modal-error");
   }
 });
 
 modal.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
-  modal.classList.remove('modal-show');
-  modal.classList.remove('modal-error');
+    modal.classList.remove('modal-down');
+    modal.classList.remove('modal-error');
   }
 });
